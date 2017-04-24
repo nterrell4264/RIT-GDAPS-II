0a2badbfacda14ea4aac41ba400b69e360e85535
nat7611 <nat7611@NM2550-02>
2017-04-24 10:32:47 -0400

    Week 12 ICE

---
+++ NPCL/NPCL/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TalkObject;
+
+namespace NPCL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NPC merchant = new NPC("merchant");
+            merchant.SetText(0, "Welcome to my shop...");
+            merchant.SetText(1, "I know you! I warned you about stealing from me!");
+            merchant.SetText(2, "Prepare to pay the ultimate price!");
+            foreach (string textbox in merchant.textboxes) Console.WriteLine("\"" + textbox + "\"");
+        }
+    }
+}
---
+++ NPCL/TalkObject/NPC.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TalkObject
+{
+    public class NPC
+    {
+        public string Name { get; private set; }
+        public String[] textboxes { get; private set; }
+
+        public NPC(string name)
+        {
+            Name = name;
+            textboxes = new string[3];
+        }
+
+        public void SetText(int boxNum, string message)
+        {
+            if (boxNum >= 0 && boxNum < textboxes.Length)
+                textboxes[boxNum] = message;
+            else throw new IndexOutOfRangeException();
+        }
+    }
+}
