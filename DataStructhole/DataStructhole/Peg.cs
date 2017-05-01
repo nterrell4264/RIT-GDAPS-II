37e12898d92b01711a4488e42cb149dde109342e
Nathan <Nathan@NATHANPC>
2017-05-01 18:14:52 -0400

    This kind of works. The threads are all using the same puzzle for some reason, though.

---
+++ DataStructhole/DataStructhole/Peg.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructhole
+{
+    class Peg
+    {
+        public bool empty;
+        public int Index { get; private set; }
+        public Dictionary<Peg,Peg> moves; //Key peg is the adjacent one, value peg is one after that.
+
+        public Peg(int slot)
+        {
+            Index = slot;
+            empty = false;
+            moves = new Dictionary<Peg, Peg>();
+        }
+
+        public Peg KeyOf(Peg peg)
+        {
+            foreach (KeyValuePair<Peg, Peg> move in moves)
+                if (move.Value.Equals(peg)) return move.Key;
+            return null;
+        }
+    }
+}
---
+++ DataStructhole/DataStructhole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DataStructhole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 14; i++) {
+                PegPuzzle puzzle = new PegPuzzle(i);
+                Thread thread = new Thread(new ThreadStart(puzzle.SolvePuzzle));
+                thread.Start();
+            };
+        }
+    }
+}
